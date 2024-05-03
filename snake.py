434f1c001a0a66498e3306139c7aac8a5aa58e1f
Mile121 <milevretoski@icloud.com>
2024-05-03 11:01:40 +0200

    hur ormens längd ändras

--- snake.py
+++ snake.py
@@ -61,3 +61,38 @@ class Snake():
             return True 
 
         return False
+    def getKey(self, event):
+
+        if event.char == "w" or event.char == "d" or event.char == "s" or event.char == "a" or event.char == " ":
+            self.key = event.char
+
+    def getSnakeX(self, index):
+        return self.snakeX[index]
+
+    def getSnakeY(self, index):
+        return self.snakeY[index]
+
+    def getSnakeLength(self):
+        return self.snakeLength
+
+    def getPoints(self):
+        return self.points
+
+
+class Apple:
+
+    def __init__(self):
+        self.appleX = random.randint(1, boardWidth - 2)
+        self.appleY = random.randint(1, boardHeight - 2)
+
+    def getAppleX(self):
+        return self.appleX
+
+    def getAppleY(self):
+        return self.appleY
+
+    def createNewApple(self):
+        self.appleX = random.randint(1, boardWidth - 2)
+        self.appleY = random.randint(1, boardHeight - 2)
+
+class GameLoop:
